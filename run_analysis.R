11aba3bae1a98bdadc6de2824ad42670d603f593
Vallari Shende <66207248+VallariS@users.noreply.github.com>
2020-07-07 16:52:55 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -21,28 +21,34 @@ subject_train<-read.table("UCI HAR Dataset\\train\\subject_train.txt",col.names=
 x_train<-read.table("UCI HAR Dataset\\train\\X_train.txt",col.names=features$functions)
 y_train<-read.table("UCI HAR Dataset\\train\\y_train.txt",col.names="labels")
 
-analyzedata<-function(){
-    X<-rbind(x_test,x_train)
-    Y<-rbind(y_test,y_train)
-    Subject<-rbind(subject_test,subject_train)
-    mergeddf<-cbind(Subject,Y,X)
-    mergedtbl<-tbl_df(mergeddf)
-    wanted_data<-select(mergedtbl,subject,labels,contains("mean"),contains("std"))
-    wanted_data$labels<-activities[wanted_data$labels,2]
-    names(wanted_data)[2]<-"activity"
-    names(wanted_data)<-gsub("Acc","Accelerometer",names(wanted_data))
-    names(wanted_data)<-gsub("Gyro","Gyroscope",names(wanted_data))
-    names(wanted_data)<-gsub("BodyBody","Body",names(wanted_data))
-    names(wanted_data)<-gsub("Mag","Magnitude",names(wanted_data))
-    names(wanted_data)<-gsub("^t","Time",names(wanted_data))
-    names(wanted_data)<-gsub("^f","Frequency",names(wanted_data))
-    names(wanted_data)<-gsub("tBody","TimeBody",names(wanted_data))
-    names(wanted_data)<-gsub("-mean()","Mean",names(wanted_data),ignore.case = TRUE)
-    names(wanted_data)<-gsub("-std()","STD",names(wanted_data),ignore.case = TRUE)
-    names(wanted_data)<-gsub("-freq()","Frequency",names(wanted_data),ignore.case = TRUE)
-    names(wanted_data)<-gsub("angle","Angle",names(wanted_data))
-    names(wanted_data)<-gsub("gravity","Gravity",names(wanted_data))
-    
-    summary_data<- wanted_data %>% group_by(subject,activity) %>% summarise_all(list(mean = mean))
-    write.table(summary_data,"UCI HAR Dataset\\FinalData.txt",row.names = FALSE)
-}
+##Merging the training and the test sets to create one data set
+X<-rbind(x_test,x_train)
+Y<-rbind(y_test,y_train)
+Subject<-rbind(subject_test,subject_train)
+mergeddf<-cbind(Subject,Y,X)
+mergedtbl<-tbl_df(mergeddf)
+
+##Extracting the measurements on the mean and standard deviation for each measurement
+wanted_data<-select(mergedtbl,subject,labels,contains("mean"),contains("std"))
+
+##Using descriptive activity names to name the activities in the data set
+wanted_data$labels<-activities[wanted_data$labels,2]
+
+##Appropriately labelling the data set with descriptive variable names
+names(wanted_data)[2]<-"activity"
+names(wanted_data)<-gsub("Acc","Accelerometer",names(wanted_data))
+names(wanted_data)<-gsub("Gyro","Gyroscope",names(wanted_data))
+names(wanted_data)<-gsub("BodyBody","Body",names(wanted_data))
+names(wanted_data)<-gsub("Mag","Magnitude",names(wanted_data))
+names(wanted_data)<-gsub("^t","Time",names(wanted_data))
+names(wanted_data)<-gsub("^f","Frequency",names(wanted_data))
+names(wanted_data)<-gsub("tBody","TimeBody",names(wanted_data))
+names(wanted_data)<-gsub("-mean()","Mean",names(wanted_data),ignore.case = TRUE)
+names(wanted_data)<-gsub("-std()","STD",names(wanted_data),ignore.case = TRUE)
+names(wanted_data)<-gsub("-freq()","Frequency",names(wanted_data),ignore.case = TRUE)
+names(wanted_data)<-gsub("angle","Angle",names(wanted_data))
+names(wanted_data)<-gsub("gravity","Gravity",names(wanted_data))
+
+##From the data set in step 4, creating a second, independent tidy data set with the average of each variable for each activity and each subject.
+summary_data<- wanted_data %>% group_by(subject,activity) %>% summarise_all(list(mean = mean))
+write.table(summary_data,"UCI HAR Dataset\\FinalData.txt",row.names = FALSE)
